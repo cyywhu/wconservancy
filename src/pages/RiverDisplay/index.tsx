345325e2f9a17aaf9d7fa920be360b121ae843bc
陈宥伊 <chenyouyi@youzhiguideMacBook-Pro.local>
2021-12-06 17:08:06 +0800

    debug

--- src/pages/RiverDisplay/index.tsx
+++ src/pages/RiverDisplay/index.tsx
@@ -3,50 +3,35 @@ import { useState, useEffect } from 'react';
 import styles from './index.less';
 
 const RiverDisplay = () => {
-  const [renderer, setRenderer] = useState<THREE.WebGLRenderer>(new THREE.WebGLRenderer({antialias: true}));
-  const [scene, setScene] = useState<THREE.Scene>(new THREE.Scene());
-  const [camera, setCamera] = useState<THREE.PerspectiveCamera>(new THREE.PerspectiveCamera());
+  // 创建一个场景、相机、渲染器
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, 400 / 400, 0.1, 1000);
+  camera.position.z = 3;
+  const renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(400, 400);
 
   useEffect(() => {
-    // init
-    const width = 400;
-    const height = 400;
-    renderer.setSize(width, height);
+  // 绑定一个node展示
+  if(document.getElementById('canvas-frame')){
     const node: HTMLElement = document.getElementById('canvas-frame')!;
     node.appendChild(renderer.domElement);
-    renderer.setClearColor(0x000000, 1.0);
 
-    // init camera
-    const newCamera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-    setCamera(newCamera);
-    camera.position.set(400,400,400)
-    camera.up.set(0,1,0);
-    camera.lookAt(0,0,0);
+    // 创建一个3D物体加入场景
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial ({color: 'blue'});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    // init scene
-    //const scene = new THREE.Scene();
-
-    // init light
-    const light = new THREE.AmbientLight(0xffffff);
-    light.position.set(300, 300, 0);
-    scene.add(light);
-
-    // init object
-    var geometry = new THREE.BoxGeometry(100, 100, 100);
-    var material = new THREE.MeshLambertMaterial({color:0x0000ff});
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, 0);
-    scene.add(mesh);
-
-    //renderer.render(scene, camera);
+    // 创建动画
+    function animation(){          
+      requestAnimationFrame(animation);
+      renderer.render(scene, camera);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    }
     animation();
-
+  }
   }, [])
-
-  const animation = () => {         
-    renderer.render(scene, camera);
-    requestAnimationFrame(animation);
-}
 
   return (
     <div id={'canvas-frame'} className={styles.canvas} />
